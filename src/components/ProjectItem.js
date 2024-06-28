68f36938dd3b8d1dcabbdd43fb5e2d50e44dfd6e
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-06-28 14:31:11 +0300

    react key lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let techElement 
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {/* <div className="technologies">
+        {technologies.map(function(technology, index){
+          
+          <span key = {index} >{technology}</span>
+        })} */}
+        
+        <div className="technologies">
+  {technologies.map((technology, index) => (
+    <span key={index}>{technology}</span>
+  ))}
+
+
       </div>
     </div>
   );
